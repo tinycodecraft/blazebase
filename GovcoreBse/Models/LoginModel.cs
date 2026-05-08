9da60892e9621eaca4d7755e86adfde36b36bbc3
tinycodecraft <raymond.cw.yau@gmail.com>
2026-05-08 17:50:09 +0800

    adjust the tooltip

--- GovcoreBse/Models/LoginModel.cs
+++ GovcoreBse/Models/LoginModel.cs
@@ -5,9 +5,11 @@ namespace GovcoreBse.Models
     public class LoginModel
     {
         [Required]
+        [Display(Name ="UserId",ResourceType = typeof(GovcoreBse.Resources.SharedResource))]
         public string UserId { get; set; }
 
         [Required, MinLength(5)]
+        [Display(Name = "Password", ResourceType = typeof(GovcoreBse.Resources.SharedResource))]
         public string Password { get; set; }
     }
 }
